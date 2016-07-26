063b6cc7ba4c65a66620a4246e0bf0d7a661f324
bowhan <bowhan@icloud.com>
2016-07-26 13:26:45 -0400

    add bash wrapper

---
+++ bin/_split.sh
@@ -0,0 +1,149 @@
+#################################################################################
+# Copyright (c) 2016-, Pacific Biosciences of California, Inc.
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted (subject to the limitations in the
+# disclaimer below) provided that the following conditions are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#  notice, this list of conditions and the following disclaimer.
+#
+#  * Redistributions in binary form must reproduce the above
+#  copyright notice, this list of conditions and the following
+#  disclaimer in the documentation and/or other materials provided
+#  with the distribution.
+#
+#  * Neither the name of Pacific Biosciences nor the names of its
+#  contributors may be used to endorse or promote products derived
+#  from this software without specific prior written permission.
+#
+# NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+# GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY PACIFIC
+# BIOSCIENCES AND ITS CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL PACIFIC BIOSCIENCES OR ITS
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#################################################################################
+# author: Bo Han (bhan@pacb.com)
+# last modified: 2016-07-26 07:24
+
+#########
+# Basic #
+#########
+declare -r MODULE_NAME=Split
+declare -r MODULE_VERSION=0.0.1
+
+#########
+# Usage #
+#########
+function usage {
+cat << EOF
+${PACKAGE_NAME}/${MODULE_NAME}
+================
+
+Split PacBio BAM file with an adaptor sequence
+
+OPTIONS:
+        -h      Show usage
+        -v      Print version
+
+${REQUIRED}[ required ]
+        -i      input files with pacBio canonical smrtbell adaptor clipped, can be either of the following two
+                1. [ RSII ] directory to RSII primary analysis output, such as A01_1
+                2. [ Sequel ] subreads.bam
+
+        -p      the other smrtbell primer sequence
+        -o      Output folder, will create if not exist. Default: ${PWD}
+
+${OPTIONAL}[ optional ]
+
+${ADVANCED}[ advanced ]
+
+EOF
+echo -e "${FONT_COLOR_RESET}"
+}
+
+##########
+# Config #
+##########
+declare -a REQUIRED_PROGRAMS=('bax2bam' 'split_primer_from_pbbam')
+declare -a Inputs=()
+
+#############################
+# ARGS reading and checking #
+#############################
+while getopts "hvi:p:o:" OPTION; do
+    case $OPTION in
+        h)  usage && exit 0 ;;
+        v)  echo ${PACKAGE_NAME}${MODULE_NAME}v${CompareVersion} && exit 0;;
+        i)  declare Input=$(readlink -f ${OPTARG});
+            Inputs+=("${Input}")
+            ;;
+        p)  declare PrimerSequence=${OPTARG};;
+        o)  declare OutputDir=${OPTARG};;
+        *)  usage && exit 1;;
+    esac
+done
+declare -r DEFAULT_PRIMER='ATCTCTCTCAATTTTTTTTTTTTTTTTTTTTTTTAAGAGAGAGAT'
+
+[[ -z $Input ]] && usage && echo2 "Need do provide at least one input fl_nc fasta file with -i option" error
+[[ -z $PrimerSequence ]] && declare PrimerSequence=$DEFAULT_PRIMER && echo2 "You have not provide the pipeline with a custom smrtbell primer with -p, thus to use the default primer ${DEFAULT_PRIMER}" warning
+
+[[ -z $OutputDir ]] && OutputDir=${RANDOM}.out
+mkdir -p $OutputDir || echo2 "Don't have permission to create folder $OutputDir" error
+cd $OutputDir || echo2 "Don't have permission to access folder $OutputDir" error
+( touch a && rm -f a ) || echo2 "Don't have permission to write in folder $OutputDir" error
+
+for program in "${REQUIRED_PROGRAMS[@]}"; do binCheck $program; done
+for file in "${REQUIRED_INPUT[@]}"; do fileCheck $file; done
+
+declare RUNUID=`echo $@ | md5sum | cut -d' ' -f1` # for re-run
+echo2 "Begin the pipeline ${PACKAGE_NAME} ${MODULE_NAME}"
+declare -i Step=1
+
+echo2 "Validate the input files"
+declare -a bamInputs
+
+for input in "${Inputs[@]}"; do
+    declare Prefix=$(basename ${input})
+    echo2 "Validating input ${input}"
+    if isDir ${input}; then
+        echo2 "Input is an directory, check for RS II data"
+        if ! validateRSII $input; then echo2 "invalid RS II output $input" error; fi
+        echo2 "Converting legacy RS II data to bam format"
+        declare Bax2BamArgs=$(find ${input}/Analysis_Results -name "*bax.h5"  | xargs echo)
+        if ! isFile ${Prefix}.subreads.bam; then
+            bax2bam $Bax2BamArgs -o ${Prefix} || echo2 "failed to convert bax back to bam format" error
+        else
+            echo2 "Skipping ${input} because the output ${Prefix}.subreads.bam has existed" warning
+        fi
+        bamInputs+=( "${Prefix}.subreads.bam" )
+    else
+        bamInputs+=( "${input}" )
+    fi
+done
+let Step+=1
+
+echo2 "Begin to split bam files"
+if [[ ! -f ${RUNUID}.${Step}.Done || ${RUNUID}.${Step}.Done -ot ${RUNUID}.$((Step-1)).Done ]]; then
+    for bamFile in "${bamInputs[@]}"; do
+        declare Prefix=$(basename ${bamFile%.subreads.bam})
+        echo2 "Split ${bamFile}"
+        if ! isFile ${Prefix}.refarm.bam; then
+            split_primer_from_pbbam -p $PrimerSequence -o ${Prefix} ${bamFile}
+        else
+            echo2 "Skipping ${bamFile} because the output ${Prefix}.refarm.bam has existed" warning
+        fi
+    done
+fi
+let Step+=1
---
+++ bin/functions.sh
@@ -0,0 +1,77 @@
+# author: Bo Han (bhan@pacb.com)
+
+function usage {
+    export BOLD=`echo -ne '\e[1m'`;
+    export UNDERLINE=`echo -ne '\e[4m'`;
+    export BLINK=`echo -ne '\e[5m'`;
+    export RESET=`echo -ne '\e[0m'`;
+
+    cat << EOF
+==================
+${RESET}${BOLD}${PACKAGE_NAME}${RESET}
+==================
+This is a pipeline for the pacBio CRISPR project, which used heterogeneous
+smrtbell adaptor.
+
+[ split ]
+    Compare assembled transcriptome with annotation
+
+EOF
+}
+
+
+export ISO_8601='%Y-%m-%d %H:%M:%S %Z'
+function echo2 {
+case $2 in
+    error|e)
+        echo -e $FONT_COLOR_RED"[`date "+$ISO_8601"`] Error: $1${FONT_COLOR_RESET}" >&2 && exit 1;;
+    warning|w)
+        echo -e $FONT_COLOR_MAGENTA"[`date "+$ISO_8601"`] Warning: $1${FONT_COLOR_RESET}" >&2;;
+    *)
+        echo -e $FONT_COLOR_GREEN"[`date "+$ISO_8601"`] $1${FONT_COLOR_RESET}" >&2;;
+esac
+}
+
+
+function binCheck {
+    program_path=`which $1 2>/dev/null`
+    [[ $? -ne 0 ]] && echo2 "Required program \"$1\" is not available" error
+    [[ $VERBOSE -gt 0 ]] && echo2 "$1: $program_path"
+}
+
+
+function isFile {
+    [[ ! -f "${1}" ]] && return 1
+    return 0
+}
+
+
+function isDir {
+    [[ ! -d "${1}" ]] && return 1
+    return 0
+}
+
+
+function indexOf {
+    local element
+    local i=0
+    for element in "${@:2}"; do
+        [[ "$element" == "$1" ]] && echo $i && return 0
+        let i+=1
+    done
+    echo -1 && return 1
+}
+
+function validateRSII {
+    # TODO: need more robust validation
+    [[ ! -d $1/Analysis_Results ]] && return 1
+    return 0
+}
+
+export -f usage
+export -f echo2
+export -f binCheck
+export -f isFile
+export -f isDir
+export -f indexOf
+export -f validateRSII
